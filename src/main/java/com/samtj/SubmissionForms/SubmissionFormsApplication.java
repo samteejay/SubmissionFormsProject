0bde40920fe249279408020a7579109b95cb7e5d
Samuel Tijani <omalitijani@gmail.com>
2020-02-04 20:25:32 +0100

    First commit

---
+++ src/main/java/com/samtj/SubmissionForms/SubmissionFormsApplication.java
@@ -0,0 +1,13 @@
+package com.samtj.SubmissionForms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmissionFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmissionFormsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samtj/SubmissionForms/SubmissionFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samtj.SubmissionForms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmissionFormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
