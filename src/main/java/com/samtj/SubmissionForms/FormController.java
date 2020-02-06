7cd9c16484c0d337c2926c1ec22ca85fa1768476
Samuel Tijani <omalitijani@gmail.com>
2020-02-06 10:30:36 +0100

    Add REST CRUD operations

--- src/main/java/com/samtj/SubmissionForms/FormController.java
+++ src/main/java/com/samtj/SubmissionForms/FormController.java
@@ -2,10 +2,11 @@ package com.samtj.SubmissionForms;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class FormController {
@@ -37,4 +38,33 @@ public class FormController {
        return mv;
     }
 
+    @RequestMapping("/customers")
+    @ResponseBody
+    public List<Customers> getCustomers() {
+        return repo.findAll();
+    }
+
+    @RequestMapping("/customers/{cid}")
+    @ResponseBody
+    public Optional<Customers> getOneCustomer(@PathVariable("cid") int cid) {
+        return repo.findById(cid);
+    }
+
+    @PostMapping("/customers")
+    public Customers saveCustomer(@RequestBody Customers customers) {
+        repo.save(customers);
+        return customers;
+    }
+
+    @DeleteMapping("/customers/{cid}")
+    public Customers deleteCustomer(@PathVariable("cid") int cid) {
+        Customers cust = repo.getOne(cid);
+        return cust;
+    }
+
+    @PutMapping(path = "/customers", consumes = {"application/json"})
+    public Customers editCustomer(@RequestBody Customers customers) {
+        repo.save(customers);
+        return customers;
+    }
 }
